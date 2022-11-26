4256164ce5f40a602ed9f6f54af7d3c7bbe246fb
Johannes Horst <j.horst@oth-aw.de>
2022-11-26 22:01:14 +0100

    ngx admin template, without examples [REQUIRES node version 14.15.0]
    the ngx admin template
    removed all example pages
    the entire raw template is available in the .zip file
    
    WARNING: node version 14.15.0 required
    use the Node Version Manager to downgrade your node version:
    https://github.com/nvm-sh/nvm
    
    Former-commit-id: 98750156dd4ae7ac830e6e8592552fa8ea2c666e

---
+++ frontend/src/app/pages/pages-menu.ts
@@ -0,0 +1,9 @@
+import { NbMenuItem } from '@nebular/theme';
+
+export const MENU_ITEMS: NbMenuItem[] = [
+  {
+    title: 'IoT Dashboard',
+    icon: 'home-outline',
+    link: '/pages/iot-dashboard',
+  }
+];
