252654dc9b44da39f75442976183434f3ff2d5a7
Patrick Sabau <d7d9@oth-aw.de>
2022-12-06 16:18:17 +0100

    Add Tests and Fix Buges

--- backend/backend/opcua_client.py
+++ backend/backend/opcua_client.py
@@ -17,24 +17,28 @@ class OPCUAClient:
         Returns:
             list[Sensor]: All Sensors in OPCUA Server as a list
         """
-        self.client.connect()
-        root = self.client.get_root_node()
-        objects_folder = root.get_children()[0]
-        sensornetzwerk = objects_folder.get_children()[1]
-        sensornodes = sensornetzwerk.get_children()
+        try:
+            self.client.connect()
+            root = self.client.get_root_node()
+            objects_folder = root.get_children()[0]
+            sensornetzwerk = objects_folder.get_children()[1]
+            sensornodes = sensornetzwerk.get_children()
 
-        sensorObj = []
-        #creating a sensor object for each sensor
-        for sensornode in sensornodes:
-            for sensor_in_node in sensornode.get_children():
-                for sensorTyp in sensor_in_node.get_children():
-                    tempSensor = sensor.Sensor(sensornode = sensornode.get_display_name().Text,
-                                                sensorname = sensor_in_node.get_display_name().Text,
-                                                sensortyp = sensorTyp.get_display_name().Text,
-                                                unit = sensorTyp.get_children()[0].get_value(), 
-                                                valueNode = sensorTyp.get_children()[1])
-                    sensorObj.append(tempSensor)
-        return sensorObj
+            sensorObj = []
+            #creating a sensor object for each sensor
+            for sensornode in sensornodes:
+                for sensor_in_node in sensornode.get_children():
+                    for sensorTyp in sensor_in_node.get_children():
+                        tempSensor = sensor.Sensor(sensornode = sensornode.get_display_name().Text,
+                                                    sensorname = sensor_in_node.get_display_name().Text,
+                                                    sensortyp = sensorTyp.get_display_name().Text,
+                                                    unit = sensorTyp.get_children()[0].get_value(), 
+                                                    valueNode = sensorTyp.get_children()[1])
+                        sensorObj.append(tempSensor)
+            return sensorObj
+        except:
+            print("Connection to OPCUA Server could not be established")
+            return []
     
     def get_sensor_names(self):
         return [s.get_sensor_name() for s in self.sensors]
--- backend/tests/test_backend.py
+++ backend/tests/test_backend.py
@@ -1,7 +1,9 @@
 import backend
+from tests.test_data import test_data
 
 import pytest
 import pymongo
+import datetime
 from fastapi import FastAPI
 from fastapi.testclient import TestClient
 
@@ -16,9 +18,11 @@ def setup_module(module):
     print('Clear Table test_table')
     backend.database.conn['test_table'].sensors.drop()
 
-    print("Setting the DB Table to 'test_table'")
-    backend.sensors_routes.db_categories= backend.database.conn['test_table'].sensors
+    print("Insert Test data into db")
+    backend.database.conn['test_table'].sensors.insert_many(test_data)
 
+    print("Setting the DB Table for the backend to 'test_table'")
+    backend.sensor_routes.db_sensors = backend.database.conn['test_table'].sensors
 
 ##############################################
 #----------------BASIC TESTS------------------
@@ -36,6 +40,85 @@ def test_base_path():
     response = client.get('/')
     assert response.status_code == 200
     assert response.json() == f"OPC-UA Sensornetz, Version: {backend.config.VERSION}"
+
+##############################################
+#-------------Test Get Routes-----------------
+##############################################
+def test_sensor_names():
+    """GET('/sensornames') should get all sensor names"""
+    response = client.get('/sensornames')
+    assert response.status_code == 200
+
+
+def test_current_values():
+    """GET('/sensorvalues/current')should not fail, even if no connection to a OPCUA Server is established"""
+    response = client.get('/sensorvalues/current')
+    assert response.status_code == 200
+
+def test_timestamp_filter():
+    """GET('/sensorvalues') with timestamps"""
+    start_time = "2022-01-01T10:10:30.000000"
+    end_time = "2022-01-01T10:11:30.000000"
+    response = client.get(f'/sensorvalues?startTimestamp={start_time}&endTimestamp={end_time}')
+    assert response.status_code == 200
+    #only one datapoint matches the time
+    assert len(response.json())==1 
+
+    late_start_time = "2022-01-01T10:10:30.000000"
+    response = client.get(f'/sensorvalues?startTimestamp={late_start_time}')
+    assert response.status_code == 200
+    #filter matches all except 2
+    assert len(response.json())==2
+
+    early_end_time = "2022-01-01T10:10:30.000000"
+    response = client.get(f'/sensorvalues?endTimestamp={early_end_time}')
+    assert response.status_code == 200
+    #filter should exclude two
+    assert len(response.json())==len(test_data)-2
+
+
+def test_string_filter():
+    """GET('/sensorvalues') with different filters"""
+    response = client.get('/sensorvalues')
+    assert response.status_code == 200
+
+    #test sensornode
+    sensornode_param='SensorNode_1'
+    test_result = [data_point for data_point in test_data if data_point['sensornode']==sensornode_param]
+    response = client.get(f'/sensorvalues?sensornode={sensornode_param}')
+    assert response.status_code == 200
+    assert len(response.json())== len(test_result)
+
+    #test sensorname
+    sensorname_param ='ABC123'
+    test_result = [data_point for data_point in test_data if data_point['sensorname']==sensorname_param]
+    response = client.get(f'/sensorvalues?sensorname={sensorname_param}')
+    assert response.status_code == 200
+    assert len(response.json())== len(test_result)
+    
+    #test sensortyp
+    sensortyp_param = 'AirPressure'
+    test_result = [data_point for data_point in test_data if data_point['sensortyp']==sensortyp_param]
+    response = client.get(f'/sensorvalues?sensortyp={sensortyp_param}')
+    assert response.status_code == 200
+    assert len(response.json())== len(test_result)
+
+    #combine params
+    test_result = [data_point for data_point in test_data if data_point['sensornode']==sensornode_param]
+    test_result = [data_point for data_point in test_result if data_point['sensorname']==sensorname_param]
+    test_result = [data_point for data_point in test_result if data_point['sensortyp']==sensortyp_param]
+    response = client.get(f'/sensorvalues?sensornode={sensornode_param}&sensorname={sensorname_param}&sensortyp={sensortyp_param}')
+    assert response.status_code == 200
+    assert len(response.json())== len(test_result)
+
+def test_combined_filters():
+    """GET('/sensorvalues') with combined filters"""
+    uri = "/sensorvalues?sensornode=SensorNode_1&sensorname=ABC123&sensortyp=AirPressure&startTimestamp=2022-01-01T10:10:30.000000&endTimestamp=2022-01-01T10:11:30.000000"
+    response = client.get(uri)
+    assert response.status_code == 200
+    assert len(response.json()) == 1
+
+
 
 ##############################################
 #----------------TEARDOWN---------------------
