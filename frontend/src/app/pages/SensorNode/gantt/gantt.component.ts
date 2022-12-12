9c1f2b4093df3d828466eeea9aa9ebcbfe8d2830
Saniye Ogul <saniye_ogul@hotmail.de>
2022-12-12 21:30:21 +0100

    Gantt 2.0
    Neues Gantt Model wird ausprobiert aber echarts.graphic ist undefiniert (aus längere Recherche könnte es sein, dass diese Funktionalität in Typescript nicht unterstützt wird) --> eventuelle Lösung: chart.js ausprobieren als echarts

--- frontend/src/app/pages/SensorNode/gantt/gantt.component.ts
+++ frontend/src/app/pages/SensorNode/gantt/gantt.component.ts
@@ -1,4 +1,4 @@
-import {ChangeDetectionStrategy, Component, Input, OnChanges, OnDestroy, SimpleChanges} from '@angular/core';
+import {ChangeDetectionStrategy, Component, ElementRef, Input, OnChanges, OnDestroy, SimpleChanges, ViewChild} from '@angular/core';
 import { NbThemeService } from '@nebular/theme';
 import { takeWhile } from 'rxjs/operators' ;
 import { SolarData } from '../../../@core/data/solar';
@@ -12,9 +12,11 @@ import * as moment from 'moment';
   styleUrls: ['./gantt.component.scss']
 })
 export class GanttComponent {
+  
 
 
   echartsIntance: any;
+  @ViewChild('gantt') span:ElementRef;
 
   constructor(private theme: NbThemeService,) {}
 
@@ -27,8 +29,10 @@ export class GanttComponent {
 
   options: EChartsOption = {};
   themeSubscription: any;
+  
 
   ngAfterViewInit() {
+
 
     this.themeSubscription = this.theme.getJsTheme().subscribe(config => {
 
@@ -41,116 +45,136 @@ export class GanttComponent {
       //https://stackoverflow.com/questions/73300725/apache-echarts-schedule-style-chart-layout
       //https://echarts.apache.org/examples/en/editor.html?c=custom-profile&reset=1&edit=1
 
-      const valveColors = [
-        "#f59527",
-        "#00e200",
-        "#2da8f3",
-    ]
-    
-    var data = [
-      {
-          "name": "Valve 1",
-          "value": [
-              1655647200000,
-              1657980000000,
-              0
-          ]
-      },
-      {
-          "name": "Valve 3",
-          "value": [
-              1657980000000,
-              1659448800000,
-              2
-          ]
-      },
-      {
-          "name": "Valve 1",
-          "value": [
-              1659448800000,
-              1660526144467,
-              0
-          ]
-      },
-      {
-          "name": "Valve 2",
-          "value": [
-              1655647200000,
-              1660526144467,
-              1
-          ]
+
+      
+
+      var data = [];
+      var dataCount = 10;
+      var startTime = +new Date();
+      var categories = ['categoryA', 'categoryB', 'categoryC'];
+      var types = [
+        { name: 'JS Heap', color: '#7b9ce1' },
+        { name: 'Documents', color: '#bd6d6c' },
+        { name: 'Nodes', color: '#75d874' },
+        { name: 'Listeners', color: '#e0bc78' },
+        { name: 'GPU Memory', color: '#dc77dc' },
+        { name: 'GPU', color: '#72b362' }
+      ];
+      // Generate mock data
+      categories.forEach(function (category, index) {
+        var baseTime = startTime;
+        for (var i = 0; i < dataCount; i++) {
+          var typeItem = types[Math.round(Math.random() * (types.length - 1))];
+          var duration = Math.round(Math.random() * 10000);
+          data.push({
+            name: typeItem.name,
+            value: [index, baseTime, (baseTime += duration), duration],
+            itemStyle: {
+              normal: {
+                color: typeItem.color
+              }
+            }
+          });
+          baseTime += Math.round(Math.random() * 2000);
+        }
+      });
+      function renderItem(params, api) {
+        var categoryIndex = api.value(0);
+        var start = api.coord([api.value(1), categoryIndex]);
+        var end = api.coord([api.value(2), categoryIndex]);
+        var height = api.size([0, 1])[1] * 0.6;
+
+        var rectShape;
+
+        /*
+        var rectShape = echarts.graphic.clipRectByRect(
+          {
+            x: start[0],
+            y: start[1] - height / 2,
+            width: end[0] - start[0],
+            height: height
+          },
+          {
+            x: params.coordSys.x,
+            y: params.coordSys.y,
+            width: params.coordSys.width,
+            height: params.coordSys.height
+          }
+        );
+        */
+        return (
+          rectShape && {
+            type: 'rect',
+            transition: ['shape'],
+            shape: rectShape,
+            style: api.style()
+          }
+        );
       }
-    ];
+
 
       this.options = {
-        xAxis: {
-          type: "time",
-  //Min is getting values from index 1, not sure why
-          min: range => range.min - (7 * 24 * 60 * 60 * 1000), //Subtract 7 days
-      },
-      yAxis: {
-          type: "category",
-          data: [ "Valve 3", "Valve 2", "Valve 1" ]
-      },
-      series: [
-          {
-              type: "custom",
-              renderItem: (params, api) => {
-                  var catIndex = api.value(2);
-                  var timeSpan = [api.value(0), api.value(1)];
-                  var start = api.coord([timeSpan[0], 2 - catIndex]);
-                  var end = api.coord([timeSpan[1], 2 -catIndex]);
-                  var size = api.size([0,1]);
-                  var height = size[1] * 0.6;
-                  //echarts = undefined Fehler taucht auf (zeichnet die balken die gefüllt werden)
-                  /*var rect = echarts.graphic.clipRectByRect(
-                      { x: start[0], y: start[1] - height / 2, width: end[0] - start[0], height: height},
-                      { x: params.coordSys.x, y: params.coordSys.y, width: params.coordSys.width, height: params.coordSys.height}
-                  );
-                  return (
-                      rect && {
-                          type: "rect",
-                          transition: ["shape"],
-                          shape: rect,
-                          style: {
-                              fill: valveColors[catIndex],
-                          },
-                      }
-                  );
-                  */
-              },
-              encode: {
-                  x: [0,1],
-                  y: 0,
-              },
-              data: data,
+        tooltip: {
+          formatter: function (params) {
+            return params.marker + params.name + ': ' + params.value[3] + ' ms';
           }
-      ],
-      tooltip: {
-          show: true,
-          trigger: "item",
-          formatter: params => {
-              return `${params.data.name}<br/> ${params.data.value[0]} - ${params.data.value[1]}` //Unix timestamps should be converted to readable dates
-          }
-      },
-      dataZoom: [
+        },
+        title: {
+          text: 'Profile',
+          left: 'center'
+        },
+        dataZoom: [
           {
-              type: "slider",
-              filterMode: "none"
+            type: 'slider',
+            filterMode: 'weakFilter',
+            showDataShadow: false,
+            top: 400,
+            labelFormatter: ''
           },
-      ],
+          {
+            type: 'inside',
+            filterMode: 'weakFilter'
+          }
+        ],
+        grid: {
+          height: 300
+        },
+        xAxis: {
+          min: startTime,
+          scale: true,
+          axisLabel: {
+            formatter: function (val) {
+              return Math.max(0, val - startTime) + ' ms';
+            }
+          }
+        },
+        yAxis: {
+          data: categories
+        },
+        series: [
+          {
+            type: 'custom',
+            renderItem: renderItem,
+            itemStyle: {
+              opacity: 0.8
+            },
+            encode: {
+              x: [1, 2],
+              y: 0
+            },
+            data: data
+          }
+        ]
   
       };
 
-      this.refreshOptions();
-      console.log("options2", this.options);
-      
+      this.refreshOptions();      
     });
 
   }
 
   onChartInit(echarts) {
-    this.echartsIntance = echarts;
+    this.echartsIntance = echarts;    
   }
 
   
@@ -162,8 +186,6 @@ export class GanttComponent {
 
   refreshOptions() {
     if(this.echartsIntance) {
-      console.log("options2", this.options);
-
       this.echartsIntance.setOption(this.options);
     }
   }
