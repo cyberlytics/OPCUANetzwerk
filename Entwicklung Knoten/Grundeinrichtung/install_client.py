e5319a40b2146d20f981751a5aca8c928589e422
Ries, Stefan <s.ries@oth-aw.de>
2022-12-14 16:23:18 +0100

    Update for manually setting ntp

--- Entwicklung Knoten/Grundeinrichtung/install_client.py
+++ Entwicklung Knoten/Grundeinrichtung/install_client.py
@@ -8,6 +8,8 @@
 #   - Funktion zum Anlegen von Dateien überarbeitet
 # V1.0.2    14.12.2022
 #   - DNS Update für VPN ergänzt
+# V1.0.3
+#   - NTP Update für VPN ergänzt
 ### --------------------------------------------------------------------------
 
 __author__      = "Manuel Zimmermann"
@@ -355,6 +357,17 @@ def install_vpn():
     cmd("systemctl enable openvpn@client.service")                                                    # Autostart-Service aktivieren
     cmd("systemctl daemon-reload")
     cmd("service openvpn@client start")
+
+    ### NTP einrichten
+    log("Konfiguriere NTP")
+    apt_install("ntp")
+    ntp_path = Path("/etc/ntp.conf")
+    replace_in_file(ntp_path, re.compile(r'(^#?server .*?$)', re.M), r"")
+    append_in_file(ntp_path, "\nserver 192.168.60.1")
+    cmd("systemctl stop systemd-timesyncd")
+    cmd("systemctl disable systemd-timesyncd")
+    cmd("/etc/init.d/ntp stop")
+    cmd("/etc/init.d/ntp start")
 
     log("VPN Einrichtung abgeschlossen")
     
