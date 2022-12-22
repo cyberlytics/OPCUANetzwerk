f28e2e832286c699e035cd8478b63024a62c74f3
Johannes Horst <j.horst@oth-aw.de>
2022-12-22 17:44:55 +0100

    Added toggle to view Live Data

--- frontend/src/app/@theme/theme.module.ts
+++ frontend/src/app/@theme/theme.module.ts
@@ -14,6 +14,7 @@ import {
   NbThemeModule,
   NbTimepickerModule,
   NbInputModule,
+  NbToggleModule,
 } from '@nebular/theme';
 import { NbEvaIconsModule } from '@nebular/eva-icons';
 import { NbSecurityModule } from '@nebular/security';
@@ -56,7 +57,8 @@ const NB_MODULES = [
   NbSelectModule,
   NbIconModule,
   NbEvaIconsModule,
-  NbInputModule
+  NbInputModule,
+  NbToggleModule
 ];
 const COMPONENTS = [
   HeaderComponent,
