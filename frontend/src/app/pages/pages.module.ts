6a8716fdca60073fbc377812cf3d74b4406730f5
Johannes Horst <j.horst@oth-aw.de>
2023-01-04 21:18:04 +0100

    Removed example dashboard compnent and module

--- frontend/src/app/pages/pages.module.ts
+++ frontend/src/app/pages/pages.module.ts
@@ -3,7 +3,6 @@ import { NbMenuModule } from '@nebular/theme';
 
 import { ThemeModule } from '../@theme/theme.module';
 import { PagesComponent } from './pages.component';
-import { DashboardModule } from './dashboard/dashboard.module';
 import { PagesRoutingModule } from './pages-routing.module';
 import { MiscellaneousModule } from './miscellaneous/miscellaneous.module';
 import { SensorNodeDashboardModule } from './SensorNode/sensornode-dashboard.module';
@@ -13,7 +12,6 @@ import { SensorNodeDashboardModule } from './SensorNode/sensornode-dashboard.mod
     PagesRoutingModule,
     ThemeModule,
     NbMenuModule,
-    DashboardModule,
     MiscellaneousModule,
     SensorNodeDashboardModule,
   ],
